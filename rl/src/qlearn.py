0247633535bc5328d692cd4d2f1109d8f2ed1328
Yug Ajmera <yugajmera@gmail.com>
2020-06-28 17:16:19 +0530

    q learning algo

--- rl/src/qlearn.py
+++ rl/src/qlearn.py
@@ -9,30 +9,41 @@ class QLearn:
         self.actions = actions
 
     def getQ(self, state, action):
-        return self.q.get(state, action)
+        return self.q.get((state, action), 0.0)
 
     def learnQ(self, state, action, reward, value):
-        
-        oldv = self.q.get(state, action)
+        '''
+        Q-learning:
+            Q(s, a) += alpha * (reward(s,a) + max(Q(s') - Q(s,a))            
+        '''
+        oldv = self.q.get((state, action), None)
         if oldv is None:
             self.q[(state, action)] = reward
         else:
             self.q[(state, action)] = oldv + self.alpha * (value - oldv)
 
-    def chooseAction(self, state):
-        if random.random() < self.epsilon:
-            action = random.choice(self.actions)
-        else:
-            q = [self.getQ(state, a) for a in self.actions]
-            maxQ = max(q)
-            count = q.count(maxQ)
-            if count > 1:
-                best = [i for i in range(len(self.actions)) if q[i] == maxQ]
-                i = random.choice(best)
-            else:
-                i = q.index(maxQ)
+    def chooseAction(self, state, return_q=False):
+        q = [self.getQ(state, a) for a in self.actions]
+        maxQ = max(q)
 
-            action = self.actions[i]
+        if random.random() < self.epsilon:
+            minQ = min(q); mag = max(abs(minQ), abs(maxQ))
+            # add random values to all the actions, recalculate maxQ
+            q = [q[i] + random.random() * mag - .5 * mag for i in range(len(self.actions))] 
+            maxQ = max(q)
+
+        count = q.count(maxQ)
+        # In case there're several state-action max values 
+        # we select a random one among them
+        if count > 1:
+            best = [i for i in range(len(self.actions)) if q[i] == maxQ]
+            i = random.choice(best)
+        else:
+            i = q.index(maxQ)
+
+        action = self.actions[i]        
+        if return_q: # if they want it, give it!
+            return action, q
         return action
 
     def learn(self, state1, action1, reward, state2):
